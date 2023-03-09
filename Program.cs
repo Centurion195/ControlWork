e80d2bd8d86e95e4beed29d3eff36769dcc4c8a9
Centurion195 <Vadim-0211@yandex.ru>
2023-03-09 12:29:47 +0300

    Program completed

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿string ReadInt(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine()!;
+}
+
+string[] CreateAndFillArray()
+{
+    int length = int.Parse(ReadInt("Введите длину массива: "));
+    string[] array = new string[length];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = ReadInt($"Введите значение #{i + 1}/{array.Length}: ");
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]}\t");
+    Console.WriteLine();
+}
+
+string[] EditArray(string[] array)
+{
+    int length = 0;
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3) length++;
+    string[] array2 = new string[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[count] = array[i];
+            count++;
+        }
+    }
+    return array2;
+}
+
+Console.Clear();
+string[] array = CreateAndFillArray();
+PrintArray(array);
+string[] array2 = EditArray(array);
+PrintArray(array2);
